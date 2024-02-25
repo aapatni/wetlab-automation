97632d45f91d3a5bb5ec7fe95d27a867e13e5646
Nicolas Tan <nicolasztan@gmail.com>
2024-02-24 21:15:31 -0800

    WIP 1

--- starter.py
+++ starter.py
@@ -1,1 +1,32 @@
-print("hello world!")
+import opentrons
+from opentrons import protocol_api
+
+'''
+~ AUTOMATE FOR ~
+
+Processs 1:
+Picking different fluids 
+
+Process 2:
+Send to microscope for analyzing with the OpenCV algorithm
+'''
+
+class opentrons:
+    def __init__(self, protocol):
+        pass
+
+    def pick_fluids(self, name):
+        pass
+
+    def send_to_microscope(self):
+        pass
+
+
+
+def main(): # run
+    new = opentrons()
+    # opentrons.pick_fluids("water")
+    # opentrons.send_to_microscope()
+
+if __name__ == '__main__':
+    main()
